886c7f2a9b73276ac691022233c0fc652cc6c61b
Ibrar Munir <ibraran086@gmail.com>
2026-08-21 23:44:29 +0500

    del keyword

--- practice 9.py
+++ practice 9.py
@@ -5,5 +5,16 @@ name1=Name("nomi")
 print(name1.name)
 name2=Name("rehmat")
 print(name2.name)
-del name2
-print(name2.name)
+
+
+
+class Car:
+    def __init__(self,name,colour):
+        self.name=name
+        self.colour=colour
+
+car1=Car("AUDI","SILVER")
+print(car1.name)
+print(car1.colour)
+del car1.colour
+print(car1.colour)
